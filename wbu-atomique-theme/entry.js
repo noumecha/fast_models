238f24797d7846f5f4e633d1e771b6c2730bbbc8
noumecha <noumechaivan@gmail.com>
2022-05-10 11:42:48 +0100

    maj : by tmc

---
+++ wbu-atomique-theme/entry.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "mahogany_hero":
+    "./src/js/sections/mahogany_hero.js",
+  "f_news_proposition":
+    "./src/js/sections/f_news_proposition.js",
+  "f_news_in_case":
+    "./src/js/sections/f_news_in_case.js",
+  "f_news_footer":
+    "./src/js/sections/f_news_footer.js",
+  "f_news_archive":
+    "./src/js/sections/f_news_archive.js",
+  "f_news_single_showcase":
+    "./src/js/sections/f_news_single_showcase.js",
+  "f_news_hero_blog":
+    "./src/js/sections/f_news_hero_blog.js",
+  "f_news_first_service":
+    "./src/js/sections/f_news_first_service.js",
+  "f_news_filter_section":
+    "./src/js/sections/f_news_filter_section.js",
+};
+
+// export default {
+//   "formatage-models-footer1":
+//     "./src/js/sections/footers/formatage-models-footer1.js",
+// };
---
+++ wbu-atomique-theme/src/js/sections/mahogany_hero.js
@@ -0,0 +1,2 @@
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mahogany/mahogany.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/mahogany/hero.scss";
