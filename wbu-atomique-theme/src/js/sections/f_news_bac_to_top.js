ff436c507ec89442874e879de3a813ef09051768
gugledas <azabzistany@gmail.com>
2022-05-23 16:22:16 +0100

    ajout bac to top

--- wbu-atomique-theme/src/js/sections/f_news_bac_to_top.js
+++ wbu-atomique-theme/src/js/sections/f_news_bac_to_top.js
@@ -1,1 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/bac-top.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/bac-top.scss";
