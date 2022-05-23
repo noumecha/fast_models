140e38fe65e62cea88602c0d343020de5cbc359a
gugledas <azabzistany@gmail.com>
2022-05-23 16:17:54 +0100

    ajout bac to top

---
+++ src/Plugin/Layout/Sections/FnewsBacToTop.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\fast_models\Plugin\Layout\Sections;
+
+use Drupal\bootstrap_styles\StylesGroup\StylesGroupManager;
+use Drupal\formatage_models\FormatageModelsThemes;
+use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
+
+/**
+ * 
+ * f_news bac to top
+ * 
+ * @Layout (
+ *  id = "fast_model_fnews_bac_top",
+ *  label = @Translation("f_news bac to top"),
+ *  category = @Translation("fast_models"),
+ *  path = "layouts/sections",
+ *  template = "f_news_bac_to_top",
+ *  library = "fast_models/f_news_bac_to_top,
+ *  default_region = "content",
+ *  regions = {
+ *      "icon" = {
+ *          "label" = @Translation("icon"),
+ *      }
+ * }
+ * )
+ * 
+ */
+
+class FnewsFirstServiceSection extends FormatageModelsSection
+{
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) 
+    {
+        // TODO Auto-generated method stub
+        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+        $this->pluginDefinition->set('icon', drupal_get_path('module', 'fast_models') . "/icons/sections/f_news_bac_top.png");
+    }
+
+    /**
+     * 
+     * {@inheritdoc}
+     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
+     * 
+     */
+    public function build(array $regions) 
+    {
+        
+        // TODO Auto-generated method stub
+        $build = parent::build($regions);
+        FormatageModelsThemes::formatSettingValues($build);
+        
+        return $build;
+    }
+
+    /**
+     * 
+     * {@inheritdoc}
+     * 
+     */
+    public function defaultConfiguration() 
+    {
+        return parent::defaultConfiguration() + [
+            'css' => '',
+            'bac_to_top' => [
+                'builder-form' => true,
+                'info' => [
+                    'title' => 'Contenu',
+                    'loader' => 'static'
+                ],
+                'fields' => [
+                    'icon' => [
+                        'text_html' => [
+                            'label' => 'Icon',
+                            'value' => "<i class="wbu-angles-up"></i>"
+                        ]
+                    ],
+                    
+                ]
+            ]
+        ];
+    }
+
+}
