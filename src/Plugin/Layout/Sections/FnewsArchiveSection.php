5f1b1743c5a54f87d40644fb9e75953fb3743a1c
noumecha <noumechaivan@gmail.com>
2022-05-24 04:07:03 +0100

    maj : news archive +++ --by tmc

--- src/Plugin/Layout/Sections/FnewsArchiveSection.php
+++ src/Plugin/Layout/Sections/FnewsArchiveSection.php
@@ -25,41 +25,11 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *      "s_bar" = {
  *          "label" = @Translation("s_bar"),
  *      },
- *      "left_link" = {
- *          "label" = @Translation("left_link"),
+ *      "archive_left" = {
+ *          "label" = @Translation("archive_left"),     
  *      },
- *      "left_link_one" = {
- *          "label" = @Translation("left_link_one"),
- *      },
- *      "left_link_two" = {
- *          "label" = @Translation("left_link_two"),
- *      },
- *      "left_link_three" = {
- *          "label" = @Translation("left_link_three"),
- *      },
- *      "left_link_four" = {
- *          "label" = @Translation("left_link_four"),
- *      },
- *      "left_link_five" = {
- *          "label" = @Translation("left_link_five"),
- *      },
- *      "left_link_six" = {
- *          "label" = @Translation("left_link_six"),
- *      },
- *      "search_link" = {
- *          "label" = @Translation("search_link"),     
- *      },
- *      "search_link_one" = {
- *          "label" = @Translation("search_link_one"),     
- *      },
- *      "search_link_two" = {
- *          "label" = @Translation("search_link_two"),     
- *      },
- *      "search_link_three" = {
- *          "label" = @Translation("search_link_three"),     
- *      },
- *      "search_link_four" = {
- *          "label" = @Translation("search_link_four"),     
+ *      "archive_right" = {
+ *          "label" = @Translation("archive_right"),     
  *      }
  *  }
  * )
@@ -105,64 +75,19 @@ class FnewsArchiveSection extends FormatageModelsSection
     {
         return parent::defaultConfiguration() + [
             'css' => '',
-            'left_content' => [
+            'content' => [
                 'builder-form' => true,
                 'info' => [
                     'title' => 'Contenu left',
                     'loader' => 'static'
                 ],
                 'fields' => [
-                    "left_link" => [
+                    "archive_left" => [
                         'text_html' => [
-                            'label' => 'left title 1',
-                            'value' => "<a href='#'>mars 2022</a><span>(1)</span>"
+                            'label' => 'Archive Content left',
+                            'value' => ""
                         ]
                     ],
-                    "left_link_one" => [
-                        'text_html' => [
-                            'label' => 'left title 2',
-                            'value' => "<a href='#'>april 2020</a><span>(10)</span>"
-                        ]
-                    ],
-                    "left_link_two" => [
-                        'text_html' => [
-                            'label' => 'left title 3',
-                            'value' => "<a href='#'>home 2015</a><span>(8)</span>"
-                        ]
-                    ],
-                    "left_link_three" => [
-                        'text_html' => [
-                            'label' => 'left title 4',
-                            'value' => "<a href='#'>août 2019</a><span>(3)</span>"
-                        ]
-                    ],
-                    "left_link_four" => [
-                        'text_html' => [
-                            'label' => 'left title 5',
-                            'value' => "<a href='#'>janvier 2022</a><span>(12)</span>"
-                        ]
-                    ],
-                    "left_link_five" => [
-                        'text_html' => [
-                            'label' => 'left title 6',
-                            'value' => "<a href='#'>june 2023</a><span>(3)</span>"
-                        ]
-                    ],
-                    "left_link_six" => [
-                        'text_html' => [
-                            'label' => 'left title 7',
-                            'value' => "<a href='#'>october 2021</a><span>(17)</span>"
-                        ]
-                    ]
-                ]
-            ],
-            'right_content' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Contenu right',
-                    'loader' => 'static'
-                ],
-                'fields' => [
                     "s_title" => [
                         'text_html' => [
                             'label' => 'Titre',
@@ -175,36 +100,12 @@ class FnewsArchiveSection extends FormatageModelsSection
                             'value' => "<input type='text' placeholder='Search...' class='form-control'>"
                         ]
                     ],
-                    'search_link' => [
+                    "archive_right" => [
                         'text_html' => [
-                            'label' => 'lien 1',
-                            'value' => "<a href='#'>home</a>"
+                            'label' => 'Archive Content left',
+                            'value' => ""
                         ]
-                    ],
-                    'search_link_one' => [
-                        'text_html' => [
-                            'label' => 'lien 2',
-                            'value' => "<a href='#'>archive</a>"
-                        ]
-                    ],
-                    'search_link_two' => [
-                        'text_html' => [
-                            'label' => 'lien 3',
-                            'value' => "<a href='#'>home</a>"
-                        ]
-                    ],
-                    'search_link_three' => [
-                        'text_html' => [
-                            'label' => 'lien 4',
-                            'value' => "<a href='#'>posts</a>"
-                        ]
-                    ],
-                    'search_link_four' => [
-                        'text_html' => [
-                            'label' => 'lien 5',
-                            'value' => "<a href='#'>product</a>"
-                        ]
-                    ]  
+                    ]
                 ]
             ]
         ];
