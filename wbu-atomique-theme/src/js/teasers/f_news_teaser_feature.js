02827e73f434b0905b3c20972fbcc6fa9fa778b0
noumecha <noumechaivan@gmail.com>
2022-05-24 22:00:22 +0100

    maj : fix teaser scss load --by tmc

--- wbu-atomique-theme/src/js/teasers/f_news_teaser_feature.js
+++ wbu-atomique-theme/src/js/teasers/f_news_teaser_feature.js
@@ -1,1 +1,1 @@
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/featured-section.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/molecule/teaser/feature-teaser.scss";
