a13ed67de32d9ad587cc5f0316136e1f77d315da
noumecha <noumechaivan@gmail.com>
2022-05-28 00:35:37 +0100

    maj : by tmc

--- src/Plugin/Layout/Sections/Menus/FastModelsFnewsFirstMenu.php
+++ src/Plugin/Layout/Sections/Menus/FastModelsFnewsFirstMenu.php
@@ -77,7 +77,7 @@ class FastModelsFnewsFirstMenu extends FormatageModelsSection {
     //
     $this->formatListMenus($build['fn_first_nav']['3ca6f07a-95f8-40e2-86e0-0fe4f860d323']['content']['#items']);
 
-    dump($this->getMenus($build['fn_first_nav']));
+    var_dump($this->getMenus($build['fn_first_nav']));
     
     return $build;
   }
