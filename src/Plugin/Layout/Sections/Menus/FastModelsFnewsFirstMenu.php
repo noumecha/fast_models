55c0f2400335212bf9e4c982772129fadac466f6
noumecha <noumechaivan@gmail.com>
2022-05-27 23:41:45 +0100

    maj: by tmc

--- src/Plugin/Layout/Sections/Menus/FastModelsFnewsFirstMenu.php
+++ src/Plugin/Layout/Sections/Menus/FastModelsFnewsFirstMenu.php
@@ -56,7 +56,7 @@ class FastModelsFnewsFirstMenu extends FormatageModelsSection {
         'first-nav'
       ]
     ];
-    $id = $build['fn_first_nav'][reset(array_keys($build['fn_first_nav']))];
+
     $build['fn_first_nav']['3ca6f07a-95f8-40e2-86e0-0fe4f860d323']['#children'] = '<div class="first-nav__brand">
             <a href="#">
                 F+ news
@@ -78,7 +78,7 @@ class FastModelsFnewsFirstMenu extends FormatageModelsSection {
     //
     $this->formatListMenus($build['fn_first_nav']['3ca6f07a-95f8-40e2-86e0-0fe4f860d323']['content']['#items']);
     
-    dump($id);
+    dump($build['fn_first_nav'][reset(array_keys($build['fn_first_nav']))]);
     return $build;
   }
   
