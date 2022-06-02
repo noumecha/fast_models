df989df3774ff3a9b0fe865bca9848f03a91337f
noumecha <noumechaivan@gmail.com>
2022-06-02 02:57:26 +0100

    maj : by tmc

--- src/Plugin/Layout/Sections/Menus/FastModelsFnewsSecondMenu.php
+++ src/Plugin/Layout/Sections/Menus/FastModelsFnewsSecondMenu.php
@@ -78,7 +78,7 @@ class FastModelsFnewsSecondMenu extends FormatageModelsSection
         foreach ($fn_scd_nav as $k => $m) {
             if (isset($m['#base_plugin_id']) && $m['#base_plugin_id'] === 'system_menu_block') {
                                
-                $fn_scd_nav[$k]['#attributes'] = [
+                /*$fn_scd_nav[$k]['#attributes'] = [
                     'class' => [
                         'sn-bloc'
                     ]
@@ -92,7 +92,7 @@ class FastModelsFnewsSecondMenu extends FormatageModelsSection
                 // set a new theme hoock () : refers to .theme.inc file
                 $fn_scd_nav[$k]['content']['#theme'] = 'layoutmenu_fast_models_fn_second_menu';
                 //
-                $this->formatListMenus($fn_scd_nav[$k]['content']['#items']);
+                $this->formatListMenus($fn_scd_nav[$k]['content']['#items']);*/
             }
         }
         return $fn_scd_nav;
