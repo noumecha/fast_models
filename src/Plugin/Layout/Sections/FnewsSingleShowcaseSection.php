5555aae82f7135d5e3cd0d12552db2018130c8e0
noumecha <noumechaivan@gmail.com>
2022-06-03 23:44:26 +0100

    maj : upd showcase -tmc

--- src/Plugin/Layout/Sections/FnewsSingleShowcaseSection.php
+++ src/Plugin/Layout/Sections/FnewsSingleShowcaseSection.php
@@ -34,9 +34,39 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *      "tag_button_one" = {
  *          "label" = @Translation("tag_button_one"),     
  *      },
- *      "sticky_author" = {
- *          "label" = @Translation("sticky_author"),     
- *      }
+ *       "sticky_author_image" = {
+ *          "label" = @Translation("sticky_author_image"),
+ *       },
+ *       "sticky_author_role" = {
+ *          "label" = @Translation("sticky_author_role"),
+ *       },
+ *       "sticky_author_pub" = {
+ *          "label" = @Translation("sticky_author_pub"),
+ *       },
+ *       "sticky_author_date_one" = {
+ *          "label" = @Translation("sticky_author_date_one"),
+ *       },
+ *       "sticky_author_update" = {
+ *          "label" = @Translation("sticky_author_update"),
+ *       },
+ *       "sticky_author_date_two" = {
+ *          "label" = @Translation("sticky_author_date_two"),
+ *       },
+ *       "sticky_author_number" = {
+ *          "label" = @Translation("sticky_author_number"),
+ *       },
+ *       "sticky_author_read" = {
+ *          "label" = @Translation("sticky_author_read"),
+ *       },
+ *       "sticky_author_status" = {
+ *          "label" = @Translation("sticky_author_status"),      
+ *       },
+ *       "sticky_author_status_one" = {
+ *          "label" = @Translation("sticky_author_status_one"),
+ *       },
+ *       "sticky_author_icon_contener" = {
+ *          "label" = @Translation("sticky_author_icon_contener"),
+ *       }
  *  }
  * )
  * 
@@ -132,10 +162,91 @@ class FnewsSingleShowcaseSection extends FormatageModelsSection
                             'value' => "<span href='#' class='link-btn'>tag 1</span>"
                         ]
                     ],
-                    'sticky_author' => [
+                    'sticky_author_image' => [
                         'text_html' => [
-                            'label' => 'Sticky',
-                            'value' => ""
+                            'label' => 'image auteur',
+                            'value' => '<a href="#"><img
+                                        src="https://demo.morethanthemes.com/flashyplus-news8/default/sites/default/files/styles/thumbnail/public/pictures/2019-08/avatar_0.jpg?itok=QV437nlh"
+                                        alt="" class=""></a>'
+                        ]
+                    ],
+                    'sticky_author_role' => [
+                        'text_html' => [
+                            'label' => 'role auteur',
+                            'value' => '<div class="author-by">by</div> admin'
+                        ]
+                    ],
+                    'sticky_author_pub' => [
+                        'text_html' => [
+                            'label' => 'publied mode',
+                            'value' => 'Published'
+                        ]
+                    ],
+                    'sticky_author_date_one' => [
+                        'text_html' => [
+                            'label' => 'date pub',
+                            'value' => '1 year ago'
+                        ]
+                    ],
+                    'sticky_author_update' => [
+                        'text_html' => [
+                            'label' => 'Update text',
+                            'value' => 'Last Updated'
+                        ]
+                    ],
+                    'sticky_author_date_two' => [
+                        'text_html' => [
+                            'label' => 'Update date',
+                            'value' => '1 year ago'
+                        ]
+                    ],
+                    'sticky_author_number' => [
+                        'text_html' => [
+                            'label' => 'Number',
+                            'value' => "1"
+                        ]
+                    ],
+                    'sticky_author_read' => [
+                        'text_html' => [
+                            'label' => 'Read',
+                            'value' => "min read"
+                        ]
+                    ],
+                    'sticky_author_status' => [
+                        'text_html' => [
+                            'label' => 'Statut 1',
+                            'value' => 'A –'
+                        ]
+                    ],
+                    'sticky_author_status_one' => [
+                        'text_html' => [
+                            'label' => 'status 2',
+                            'value' => 'A +'
+                        ]
+                    ],
+                    'sticky_author_icon_contener' => [
+                        'text_html' => [
+                            'label' => 'Image contener',
+                            'value' => '<a href="#" class="saf-social__btn">
+                                            <svg>
+                                                <use xlink:href="#facebook-f"></use>
+                                            </svg>
+                                        </a>
+                                        <a href="#" class="saf-social__btn">
+                                            <svg>
+                                                <use xlink:href="#twitter"></use>
+                                            </svg>
+                                        </a>
+                                        <a href="#" class="saf-social__btn">
+                                            <svg>
+                                                <use xlink:href="#envelope"></use>
+                                            </svg>
+                                        </a>
+                                        <a href="#" class="saf-social__btn">
+                                            <svg>
+                                                <use xlink:href="#print"></use>
+                                            </svg>
+                                        </a>'
                         ]
                     ]
                 ]
