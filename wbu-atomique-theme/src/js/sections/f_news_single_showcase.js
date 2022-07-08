0381c1fe79b7163cc27e19b0a6c056c1f28c306a
gugledas <azabzistany@gmail.com>
2022-07-08 15:15:32 +0100

    maj

--- wbu-atomique-theme/src/js/sections/f_news_single_showcase.js
+++ wbu-atomique-theme/src/js/sections/f_news_single_showcase.js
@@ -1,2 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/single-showcase.scss";
-import "./fast_models_sticky_author";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/sticky-author.js";
