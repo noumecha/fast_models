bd53421aef08e8234f591c40c516b5f9528a1938
gugledas <azabzistany@gmail.com>
2022-07-08 15:25:38 +0100

    maj

--- wbu-atomique-theme/src/js/sections/menus/fast_models_fn_first_menu.js
+++ wbu-atomique-theme/src/js/sections/menus/fast_models_fn_first_menu.js
@@ -1,2 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/header.scss";
-import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/header.js";
+//import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/header.js";
