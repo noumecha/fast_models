ccc8149377974b9e2d0923fa5f3b64afd7c6cfff
gugledas <azabzistany@gmail.com>
2022-07-08 13:52:41 +0100

    maj sticky.js

--- wbu-atomique-theme/src/js/sections/fast_models_sticky_author.js
+++ wbu-atomique-theme/src/js/sections/fast_models_sticky_author.js
@@ -1,1 +1,2 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/sticky-author.scss";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/sticky-author.js";
