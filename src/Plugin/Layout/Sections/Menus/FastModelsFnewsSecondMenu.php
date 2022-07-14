9c319e49976d17a8f336cc079af5bfd81d5d3194
kouwa stephane <kksasteph888@gmail.com>
2022-07-14 19:45:50 +0200

    MAJ

--- src/Plugin/Layout/Sections/Menus/FastModelsFnewsSecondMenu.php
+++ src/Plugin/Layout/Sections/Menus/FastModelsFnewsSecondMenu.php
@@ -23,131 +23,134 @@ use Drupal\formatage_models\Plugin\Layout\Sections\FormatageModelsSection;
  *          "label" = @Translation("fn_second_nav_content"),
  *      },
  *      "fn_second_nav_logo" = {
- *          "label" = @Translation("fn_second_nav_logo"),     
+ *          "label" = @Translation("fn_second_nav_logo"),
  *      },
  *      "fn_second_nav_search" = {
- *          "label" = @Translation("fn_second_nav_search"),     
+ *          "label" = @Translation("fn_second_nav_search"),
  *      },
  *      "fn_popup_search" = {
- *          "label" = @Translation("fn_popup_search"),     
+ *          "label" = @Translation("fn_popup_search"),
  *      }
  *   }
  * )
  *
  */
-
-class FastModelsFnewsSecondMenu extends FormatageModelsSection
-{
-
-    /**
+class FastModelsFnewsSecondMenu extends FormatageModelsSection {
+  
+  /**
    *
    * {@inheritdoc}
    * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels::__construct()
    */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
-        // TODO Auto-generated method stub
-        parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
-        $this->pluginDefinition->set('icon', drupal_get_path('module', 'fast_models') . "/icons/menus/fast_models_fn_second_menu_map.png");
-    }
-
-    /**
-     *
-     * {@inheritdoc}
-     * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
-     *
-     */
-    public function build(array $regions) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StylesGroupManager $styles_group_manager) {
+    // TODO Auto-generated method stub
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $styles_group_manager);
+    $this->pluginDefinition->set('icon', drupal_get_path('module', 'fast_models') . "/icons/menus/fast_models_fn_second_menu_map.png");
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   * @see \Drupal\formatage_models\Plugin\Layout\FormatageModels:build()
+   *
+   */
+  public function build(array $regions) {
+    
+    // TODO Auto-generated method stub
+    $build = parent::build($regions);
+    FormatageModelsThemes::formatSettingValues($build);
+    if (is_array($build['fn_second_nav_content']))
+      $build['fn_second_nav_content'] = $this->getMenus($build['fn_second_nav_content']);
+    return $build;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  private function getMenus(array $fn_scd_nav) {
+    foreach ($fn_scd_nav as $k => $m) {
+      if (isset($m['#base_plugin_id']) && $m['#base_plugin_id'] === 'system_menu_block') {
         
-        // TODO Auto-generated method stub
-        $build = parent::build($regions);
-        FormatageModelsThemes::formatSettingValues($build);
-        if (is_array($build['fn_second_nav_content']))
-            $build['fn_second_nav_content'] = $this->getMenus($build['fn_second_nav_content']);
-        return $build;
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     */
-    private function getMenus(array $fn_scd_nav) {
-        foreach ($fn_scd_nav as $k => $m) {
-            if (isset($m['#base_plugin_id']) && $m['#base_plugin_id'] === 'system_menu_block') {
-                               
-                $fn_scd_nav[$k]['#attributes'] = [
-                    'class' => [
-                        'sn-bloc'
-                    ]
-                ];
-                // set a new theme hoock () : refers to .theme.inc file
-                $fn_scd_nav[$k]['content']['#theme'] = 'layoutmenu_fast_models_fn_second_menu';
-                //
-                $this->formatListMenus($fn_scd_nav[$k]['content']['#items']);
-            }
-        }
-        return $fn_scd_nav;
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     */
-
-    private function formatListMenus(array &$items) {
-        foreach ($items as $k => $item) {
-            if (!empty($item['attributes'])) {
-                $attribute = $item['attributes'];
-                $attribute->addClass('nav-item');
-                if (!empty($item['below'])) {
-                    $this->formatListMenus($item['below']);
-                    $items[$k]['below'] = $item['below'];
-                }
-            }
-        }
-    }
-
-    /**
-     * 
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration()
-    {
-        return parent::defaultConfiguration() + [
-            'css' => '',
-            'fmfsm' => [
-                'builder-form' => true,
-                'info' => [
-                    'title' => 'Contenu',
-                    'loader' => 'static'
-                ],
-                'fields' => [
-                    'fn_second_nav_content' => [
-                        'text_html' => [
-                            'label' => 'Menu Content',
-                            'value' => ""
-                        ]
-                    ],
-                    'fn_second_nav_search' => [
-                        'text_html' => [
-                            'label' => 'Search icone',
-                            'value' => "<span class='wbu-search'></span>"
-                        ]
-                    ],
-                    'fn_second_nav_logo' => [
-                        'text_html' => [
-                            'label' => 'Logo',
-                            'value' => "<a href='#' class=''>storibon</a>"
-                        ]
-                    ],
-                    'fn_popup_search' => [
-                        'text_html' => [
-                            'label' => 'Pop Up Search',
-                            'value' => '<input type="text" class="form-control" placeholder="Search...">'
-                        ]
-                    ]
-                ]
-            ]
+        $fn_scd_nav[$k]['#attributes'] = [
+          'class' => [
+            'sn-bloc'
+          ]
         ];
+        // set a new theme hoock () : refers to .theme.inc file
+        $fn_scd_nav[$k]['content']['#theme'] = 'layoutmenu_fast_models_fn_second_menu';
+        //
+        $this->formatListMenus($fn_scd_nav[$k]['content']['#items']);
+      }
     }
-
+    return $fn_scd_nav;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  private function formatListMenus(array &$items) {
+    foreach ($items as $k => $item) {
+      if (!empty($item['attributes'])) {
+        $attribute = $item['attributes'];
+        $attribute->addClass('nav-item');
+        if (!empty($item['below'])) {
+          $this->formatListMenus($item['below']);
+          $items[$k]['below'] = $item['below'];
+        }
+      }
+    }
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return parent::defaultConfiguration() + [
+      'css' => '',
+      'derivate' => [
+        'value' => 'default',
+        'options' => [
+          'default' => 'default',
+          'second-nav--fixed' => 'menu statique'
+        ]
+      ],
+      'fmfsm' => [
+        'builder-form' => true,
+        'info' => [
+          'title' => 'Contenu',
+          'loader' => 'static'
+        ],
+        'fields' => [
+          'fn_second_nav_content' => [
+            'text_html' => [
+              'label' => 'Menu Content',
+              'value' => ""
+            ]
+          ],
+          'fn_second_nav_search' => [
+            'text_html' => [
+              'label' => 'Search icone',
+              'value' => "<span class='wbu-search'></span>"
+            ]
+          ],
+          'fn_second_nav_logo' => [
+            'text_html' => [
+              'label' => 'Logo',
+              'value' => "<a href='#' class=''>storibon</a>"
+            ]
+          ],
+          'fn_popup_search' => [
+            'text_html' => [
+              'label' => 'Pop Up Search',
+              'value' => '<input type="text" class="form-control" placeholder="Search...">'
+            ]
+          ]
+        ]
+      ]
+    ];
+  }
+  
 }
--- wbu-atomique-theme/src/js/sections/menus/fast_models_fn_second_menu.js
+++ wbu-atomique-theme/src/js/sections/menus/fast_models_fn_second_menu.js
@@ -1,3 +1,4 @@
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/molecule/menu/second-nav.scss";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/molecule/menu/search-popup.scss";
 import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/search-popup.js";
+import "@stephane888/wbu-atomique/scss/organisme/sections/theme_builder/f_news/js/second-nav.js";
